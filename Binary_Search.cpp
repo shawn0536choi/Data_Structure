8e4d72bee8aae9bceab8ef4501dde516dfa5a30d
shawn0536choi <45480492+shawn0536choi@users.noreply.github.com>
2019-01-20 23:07:05 +0900

    Add files via upload

---
+++ Binary_Search.cpp
@@ -0,0 +1,34 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int len;
+	printf("size of arr\n-->");
+	scanf("%d", &len);
+	int *arr = (int *)malloc(sizeof(int)*len);
+	printf("input array\n-->");
+	for (int i = 0; i < len; i++) {
+		scanf("%d", &arr[i]);
+	}
+	int purpose;
+	printf("What do you want to find?\n-->");
+	scanf("%d", &purpose);
+	int standard = len / 2;
+	bool found = false;
+	while (found == false) {
+		if (arr[standard] > purpose) {
+			standard = standard / 2;
+		}
+		else if (arr[standard] == purpose) {
+			found = true;
+			printf("\narr[%d] = %d", standard, purpose);
+		}
+		else {
+			standard = (standard + len) / 2;
+		}
+	}
+	return 0;
+}
+*/
